bf15ea0d1798b3b04fefe11a8e41b18809113bd8
giuseppe <g.dalo@cgsoftware.it>
2012-06-10 09:11:00 +0200

    Miglioramenti Gestione assistenza Garanzia

--- CreaProdDaRma.py
+++ CreaProdDaRma.py
@@ -23,34 +23,41 @@ class production_from_returned_lines(osv.osv_memory):
             claim_brw=  self.pool.get('crm.claim').browse(cr,uid, context['active_id'],context)
             if claim_brw:
                 for line in claim_brw.return_line_ids:
-                    if line.selected:
-                        ok = self.create_order_prod(cr, uid, line.product_id.id, line.product_returned_quantity,line.name, context) 
+                    if line.selected :
+                        ok = self.create_order_prod(cr, uid, line.product_id.id, line.product_returned_quantity,line.name,line, context) 
                 #import pdb;pdb.set_trace()
         
         return {'type': 'ir.actions.act_window_close',}
 
 
 
-    def create_order_prod(self, cr, uid, id,quantity,origin,context):
-        #import pdb;pdb.set_trace()
-        prod_obj = self.pool.get('mrp.production')
-        vals = prod_obj.default_get(cr,uid,[],context)
-        vals['product_id']=id
-        res = prod_obj.product_id_change( cr, uid, id, id, context)
-        value = res.get('value',False)
-        if value:
-            vals['product_uom']= value['product_uom']
-            vals['bom_id']= value['bom_id']
-            vals['routing_id'] = value['routing_id']
-        vals['product_qty']=quantity
-        
-        vals['origin']=origin
-        production_id = self.pool.get('mrp.production').create(cr, uid, vals, context)
-        prod_obj.action_compute(cr, uid, [ production_id])
-        workflow = netsvc.LocalService("workflow")
-        workflow.trg_validate(uid, 'mrp.production', production_id, 'button_confirm', cr)
-        res=[]
-        
+    def create_order_prod(self, cr, uid, id,quantity,origin,line,context):
+        #import pdb;pdb.set_trace()       
+        repair_obj = self.pool.get('mrp.repair')
+        origine = "RMA/"+line.claim_id.sequence
+        id_move = self.pool.get('stock.move').search(cr,uid,[('origin','=',origine)])
+        if id_move: 
+            vals = repair_obj.default_get(cr,uid,[],context)
+            vals['product_id']=id
+            res = repair_obj.onchange_product_id(cr, uid, id)
+            value = res.get('value',False)  
+            vals.update(value)    
+            vals['move_id']=  id_move[0]
+            ids = False
+            res2 = repair_obj.onchange_move_id(cr, uid, ids, id, vals['move_id'])
+            move_data = res2.get('value',False)
+            vals.update(move_data)
+            vals['origin']=origin
+            vals['production_id'] = line.production_id.id
+            vals['internal_notes'] = line.claim_id.description +'\n'+line.claim_descr+'\n'+line.claim_origine
+            vals['partner_id']= line.claim_id.partner_id.id
+            vals['invoice_method']= 'none'
+            vals['deliver_bool']=False
+            
+            for qant in range(0,int(quantity)):
+                id_rep = repair_obj.create(cr,uid,vals)
+        else:
+            raise osv.except_osv(_('Error !'), _('Devi prima creare un Movimento di Ingresso dell Articolo !'))
         return True    
 
 
